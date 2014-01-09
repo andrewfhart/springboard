27c54d346c796816dc210254086c5ed3e0364383
Andrew Hart <andrew@datafluency.com>
2014-01-09 06:39:18 -0800

    Initial script to provision environment with dependencies

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Springboard - Application skeleton for modern web development
+#
+# This script prepares the environment with the required Grunt dependencies.
+# After running this script, use the grunt-cli tool to manage your application.
+# See the README.md file for additional information.
+#
+
+GRUNT_PLUGINS="contrib-uglify contrib-clean bower-task bower-organiser connect-rewrite connect-proxy contrib-connect contrib-watch contrib-copy contrib-jshint contrib-watch"
+GRUNT_EXTRA_PLUGINS="connect-modrewrite"
+
+echo "Initializing NPM package file:"
+npm init
+
+echo "Installing Grunt:"
+npm install --save-dev grunt
+
+echo "Installing Grunt plugins:"
+for PLUGIN in $GRUNT_PLUGINS
+do
+    npm install --save-dev $PLUGIN
+done
+
+for PLUGIN in $GRUNT_EXTRA_PLUGINS
+do
+    npm install --save-dev $PLUGIN
+
+echo "Done."
+
